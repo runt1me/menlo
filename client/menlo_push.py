dd73f6bcfbc4cee7d85699f6095a53d2affee097
Ryan Thomas <ryan.thomas@lbmc.com>
2025-12-02 14:16:37 -0500

    updates client to support multiple servers on a single push

--- client/menlo_push.py
+++ client/menlo_push.py
@@ -9,7 +9,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".menlo.env"))
 
 API_KEY = os.getenv("MENLO_API_KEY")
-SERVER_URL = os.getenv("MENLO_SERVER_URL", "http://192.168.188.128:5000/push")
+
+def get_menlo_servers():
+    servers = []
+    for key, value in os.environ.items():
+        if key.startswith("MENLO_SERVER_URL"):
+            servers.append(value)
+
+    return servers
 
 def main():
     if len(sys.argv) != 2:
@@ -22,6 +29,8 @@ def main():
         print(f"Error: File not found: {file_path}")
         sys.exit(1)
 
+    MENLO_SERVERS = get_menlo_servers()
+
     if not API_KEY:
         print("Error: MENLO_API_KEY not found in .env")
         sys.exit(1)
@@ -30,16 +39,14 @@ def main():
         files = {"file": (os.path.basename(file_path), f)}
         headers = {"X-API-Key": API_KEY}
 
-        # print(f"[*] Uploading {file_path} to {SERVER_URL}...")
-        r = requests.post(SERVER_URL, headers=headers, files=files)
+        for url in MENLO_SERVERS:
+            # print(f"[*] Uploading {file_path} to {url}...")
+            r = requests.post(url, headers=headers, files=files)
 
     if r.status_code != 201:
         print(f"[!] Upload failed: HTTP {r.status_code}")
         print(r.text)
         sys.exit(1)
-
-    # print("[+] Success!")
-    # print(r.json())
 
 if __name__ == "__main__":
     main()
